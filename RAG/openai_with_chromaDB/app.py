8e22b29bb00c1bf9242281a4601f8846a02f6f2d
Swapnil <sswapnil_be20@thapar.edu>
2024-05-06 16:29:25 +0530

    Completed functions for RAG-with-ChromaDB
    Signed-off-by: Swapnil <sswapnil_be20@thapar.edu>

--- RAG/openai_with_chromaDB/app.py
+++ RAG/openai_with_chromaDB/app.py
@@ -23,6 +23,19 @@ def create_chunks():
     splits = text_splitter.split_documents(documents=documents)
     return splits
 
+
+def create_embeddings():
+    global persist_directory
+    embedding = OpenAIEmbeddings()
+    vectordb = Chroma.from_documents(
+        documents=create_chunks(),
+        embedding=embedding,
+        persist_directory=persist_directory
+    ) 
+    vectordb.persist()
+    print(f"Number of documents: {vectordb._collection.count()}")
+    return vectordb
+
 def similarity_search_with_score(question, vectordb, k=3):
     docs = vectordb.similarity_search_with_score(question, k=k)
     print("Similarity search results:")
@@ -56,5 +69,53 @@ def ask_query(vectordb,question):
     print(result["result"])
     return result["result"]
 
+def qa_chain_with_template(vectordb,question):
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    template = """Address the user as vinner and Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+    {context}
+    Question: {question}
+    Helpful Answer:"""
+    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectordb.as_retriever(),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+    )
+    result = qa_chain.invoke({"query": question})
+    print(f"Question: {question} \n Answer: {result['result']} \n")
+    return result["result"]
 
-# ::TODO::
+
+# memory = ConversationBufferMemory(
+#     memory_key="chat_history",
+#     return_messages=True
+# )
+
+# qa = ConversationalRetrievalChain.from_llm(
+#     llm,
+#     retriever=vectordb.as_retriever(),
+#     memory=memory
+# )
+# question = "how can i setup auto label printing"
+# result = qa.invoke({"question": question})
+# print(result['answer'])
+
+
+def create_chat_memory(vectordb):
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+    qa = ConversationalRetrievalChain.from_llm(
+        llm,
+        retriever=vectordb.as_retriever(),
+        memory=memory
+    )
+    return qa
+
+def ask_query_with_chat_memory(qa, question):
+    result = qa.invoke({"question": question})
+    print(f"Question: {question} \n Answer: {result['answer']} \n")
+    return result['answer']
